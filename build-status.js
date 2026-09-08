fd42ee7ccbe18b7c3390b60d058c1feccc23c70e
dakotawireless <erik@dakotawireless.net>
2026-09-08 14:13:39 -0600

    Bump build status to v0.1.21

--- build-status.js
+++ build-status.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const CURRENT_BUILD = "0.1.20";
+  const CURRENT_BUILD = "0.1.21";
   const cartLogo = document.querySelector('.cart-logo');
   if (!cartLogo) return;
 
